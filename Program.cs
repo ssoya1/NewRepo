f55ac6bd2325fd4c0d9de37c0f03b3f6d3c8052b
ssoya1 <08sophia.olefir15@gmail.com>
2022-08-19 01:12:23 +0300

    changed text

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace RepoTry
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Brave New World!");
         }
     }
 }
